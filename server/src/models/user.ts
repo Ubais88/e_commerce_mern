8c48a14fb3dbda1ce35c42678a39bc8383089a8f
Ubais88 <moubais2143@gmail.com>
2024-01-03 09:41:42 +0530

    user schema added

---
+++ server/src/models/user.ts
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  photo: string;
+  role: "admin" | "user";
+  gender: "male" | "female";
+  dob: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  // virtual attribute
+  age: number;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: [true, "Please enter ID"],
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter Name"],
+    },
+    email: {
+      type: String,
+      unique: [true, "Email Already Exits"],
+      required: [true, "Please enter Email Address"],
+      validator: validator.default.isEmail,
+    },
+    photo: {
+      type: String,
+      required: [true, "Please add Photo"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: [true, "Please enter Gender"],
+    },
+    dob: {
+      type: Date,
+      required: [true, "Please enter DOB"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.virtual("age").get(function () {
+  const today = new Date();
+  const dob = this.dob;
+  let age = today.getFullYear() - dob.getFullYear();
+
+  if (
+    today.getMonth() < dob.getMonth() ||
+    (today.getDate() === dob.getDate() && today.getDate() < dob.getDate())
+  ) {
+    age--;
+  }
+  return age;
+});
+
+export const User = mongoose.model<IUser>("User", userSchema);
